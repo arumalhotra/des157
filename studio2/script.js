19c52e567030ccee4fa4b8868b006adcbede322a
arumalhotra <arumalhotra@ucdavis.edu>
2017-01-31 11:52:28 -0800

    Add files via upload

--- studio2/script.js
+++ studio2/script.js
@@ -6,20 +6,20 @@ window.onload = function() {
 
     function processForm() {
 
-      var userBodypart = document.f.userBodypart.value;
-      var userVerb = document.f.userVerb.value;
-      var userAdjective_2= document.f.userAdjective_2.value;
-      var userActionverb = document.f.userActionverb.value;
-      var userAdjective = document.f.userAdjective.value;
-      var userNoun = document.f.userNoun.value;
-      var userName = document.f.userName.value;
-      var userColor = document.f.userColor.value;
+        var userBodypart = document.f.userBodypart.value;
+        var userVerb = document.f.userVerb.value;
+        var userAdjective_2 = document.f.userAdjective_2.value;
+        var userActionverb = document.f.userActionverb.value;
+        var userAdjective = document.f.userAdjective.value;
+        var userNoun = document.f.userNoun.value;
+        var userName = document.f.userName.value;
+        var userColor = document.f.userColor.value;
 
         var rap = document.getElementById("rap");
 
         rap.innerHTML = "Baby I like your style<br><br>" + "Grips on your " + userBodypart + "<br>Front way, back way" + "<br>You know that I don't " + userVerb + "<br>Streets not " + userAdjective_2 + "<br>But I never " + userActionverb + " away<br>" + "Even when I'm away<br>" + "OT, OT is never much " + userAdjective + " when we go OT<br>" + "I pray to make it back in one piece<br>" + "I pray, I pray<br><br>" + "That's why I need a one " + userNoun + "<br>Got the " + userName + " in my hand<br>" + "One more time 'fore I go<br>" + "I have powers taking " + userColor + " on me<br>" + "I need a one " + userNoun + "<br>Got the " + userName + " in my hand<br>" + "One more time 'fore I go<br>" + "I have powers taking " + userColor + " on me";
         rap.className = "show";
-        document.getElementById("drk").style.display= "block";
+        document.getElementById("drk").style.display = "block";
 
         return false;
     }
@@ -35,10 +35,10 @@ window.onload = function() {
         userColor.value = "";
         userAdjective_2.value = "";
         userActionverb.value = "";
-        userVerb.value= "";
+        userVerb.value = "";
 
         rap.className = "hide";
-        document.getElementById("drk").style.visibility= "hide";
+        document.getElementById("drk").style.visibility = "hide";
 
 
     }
