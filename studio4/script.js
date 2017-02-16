ce9a50d02e5daa9afe00e1ab5cfcdeef841cacba
arumalhotra <arumalhotra@ucdavis.edu>
2017-02-16 12:25:48 -0800

    Add files via upload

--- studio4/script.js
+++ studio4/script.js
@@ -4,17 +4,17 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
 
- document.getElementById("mus").onclick = function() {
-            var audio = document.getElementById("audio");
-            audio.play();
-        }
+    document.getElementById("mus").onclick = function() {
+        var audio = document.getElementById("audio");
+        audio.play();
+    }
 
- 	document.getElementById("1").onclick = function() {
+    document.getElementById("1").onclick = function() {
         document.getElementById("1995").style.visibility = "visible";
         document.getElementById("1995").style.display = "block";
     }
 
-   document.getElementById("2").onclick = function() {
+    document.getElementById("2").onclick = function() {
         document.getElementById("2000").style.visibility = "visible";
         document.getElementById("2000").style.display = "block";
         document.getElementById("1995").style.display = "none";
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     document.getElementById("3").onclick = function() {
-    	document.getElementById("1995").style.visibility = "hidden";
+        document.getElementById("1995").style.visibility = "hidden";
         document.getElementById("2000").style.visibility = "hidden";
         document.getElementById("2005").style.visibility = "visible";
         document.getElementById("1995").style.display = "none";
@@ -32,13 +32,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     }
     document.getElementById("4").onclick = function() {
-    	document.getElementById("1995").style.visibility = "hidden";
+        document.getElementById("1995").style.visibility = "hidden";
         document.getElementById("2000").style.visibility = "hidden";
         document.getElementById("2005").style.visibility = "hidden";
         document.getElementById("2015").style.visibility = "visible";
         document.getElementById("1995").style.display = "none";
         document.getElementById("2000").style.display = "none";
         document.getElementById("2005").style.display = "none";
-         document.getElementById("2015").style.display = "block";
+        document.getElementById("2015").style.display = "block";
     }
 });
