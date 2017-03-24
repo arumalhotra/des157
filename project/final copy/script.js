a628acc209ab20fdee0361e0b590ee31c5e388a0
arumalhotra <arumalhotra@ucdavis.edu>
2017-03-24 11:58:43 -0700

    Trial

---
+++ project/final copy/script.js
@@ -0,0 +1,108 @@
+// JavaScript Document
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+
+    document.getElementById("logo").onclick = function() {
+        document.getElementById("screen1").style.visibility = "visible";
+        document.getElementById("screen1").style.display = "block";
+        document.getElementById("screen7").style.display = "none";
+        document.getElementById("screen7").style.display = "hidden";
+
+    }
+
+
+document.getElementById("accept").onclick = function() {
+        document.getElementById("screen2").style.visibility = "visible";
+        document.getElementById("screen2").style.display = "block";
+        document.getElementById("screen1").style.display = "none";
+        document.getElementById("screen1").style.display = "hidden";
+
+    }
+
+document.getElementById("log").onclick = function() {
+        document.getElementById("screen3").style.visibility = "visible";
+        document.getElementById("screen3").style.display = "block";
+        document.getElementById("screen2").style.display = "none";
+        document.getElementById("screen2").style.display = "hidden";
+        document.getElementById("screen1").style.display = "none";
+        document.getElementById("screen1").style.display = "hidden";
+
+    }
+
+document.getElementById("ken").onclick = function() {
+        document.getElementById("screen4").style.visibility = "visible";
+        document.getElementById("screen4").style.display = "block";
+        document.getElementById("screen3").style.display = "none";
+        document.getElementById("screen3").style.display = "hidden";
+        document.getElementById("screen2").style.display = "none";
+        document.getElementById("screen2").style.display = "hidden";
+        document.getElementById("screen1").style.display = "none";
+        document.getElementById("screen1").style.display = "hidden";
+
+    }
+
+    document.getElementById("setting").onclick = function() {
+        document.getElementById("screen5").style.visibility = "visible";
+        document.getElementById("screen5").style.display = "block";
+        document.getElementById("screen4").style.display = "none";
+        document.getElementById("screen4").style.display = "hidden";
+        document.getElementById("screen3").style.display = "none";
+        document.getElementById("screen3").style.display = "hidden";
+        document.getElementById("screen2").style.display = "none";
+        document.getElementById("screen2").style.display = "hidden";
+        document.getElementById("screen1").style.display = "none";
+        document.getElementById("screen1").style.display = "hidden";
+
+    }
+
+    document.getElementById("logout").onclick = function() {
+        document.getElementById("screen2").style.visibility = "visible";
+        document.getElementById("screen2").style.display = "block";
+        document.getElementById("screen5").style.display = "none";
+        document.getElementById("screen5").style.display = "hidden";
+        document.getElementById("screen4").style.display = "none";
+        document.getElementById("screen4").style.display = "hidden";
+        document.getElementById("screen3").style.display = "none";
+        document.getElementById("screen3").style.display = "hidden";
+        document.getElementById("screen1").style.display = "none";
+        document.getElementById("screen1").style.display = "hidden";
+
+    }
+
+    document.getElementById("chat").onclick = function() {
+        document.getElementById("screen6").style.visibility = "visible";
+        document.getElementById("screen6").style.display = "block";
+        document.getElementById("screen5").style.display = "none";
+        document.getElementById("screen5").style.display = "hidden";
+        document.getElementById("screen4").style.display = "none";
+        document.getElementById("screen4").style.display = "hidden";
+        document.getElementById("screen3").style.display = "none";
+        document.getElementById("screen3").style.display = "hidden";
+        document.getElementById("screen2").style.display = "none";
+        document.getElementById("screen2").style.display = "hidden";
+        document.getElementById("screen1").style.display = "none";
+        document.getElementById("screen1").style.display = "hidden";
+
+    }
+
+    document.getElementById("home").onclick = function() {
+        document.getElementById("screen3").style.visibility = "visible";
+        document.getElementById("screen3").style.display = "block";
+        document.getElementById("screen6").style.display = "none";
+        document.getElementById("screen6").style.display = "hidden";
+        document.getElementById("screen5").style.display = "none";
+        document.getElementById("screen5").style.display = "hidden";
+        document.getElementById("screen4").style.display = "none";
+        document.getElementById("screen4").style.display = "hidden";
+        document.getElementById("screen3").style.display = "none";
+        document.getElementById("screen3").style.display = "hidden";
+        document.getElementById("screen2").style.display = "none";
+        document.getElementById("screen2").style.display = "hidden";
+        document.getElementById("screen1").style.display = "none";
+        document.getElementById("screen1").style.display = "hidden";
+
+    }
+
+
+});
